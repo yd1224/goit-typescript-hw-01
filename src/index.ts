26e84700e0faf0d91e73f3cddb1408cf0551ab94
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-03-11 16:59:33 +0100

    return

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,11 @@
-// import { concatenation } from './concatenation';
+import { concatenation } from './concatenation';
 
-// const button = document.querySelector('button')!;
-// const input = document.querySelector('input')!;
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
 
-// if (button && input) {
-//   button.addEventListener('click', () => {
-//     concatenation(input.value, 'hello!');
-//   });
-// }
-
-class Employee {
-  // Заповніть модифікатори доступу
- public name: string;
- private department: string;
- protected salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
-
-  getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
-  }
+if (button && input) {
+  button.addEventListener('click', () => {
+    concatenation(input.value, 'hello!');
+  });
 }
 
-class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor( name: string, department: string, salary: number){
-    super(name, department, salary)
-    this.salary += 1000;
-  }
-}
-const manger=new Manager("Ajax", "Business", 0)
-console.log(manger.getEmployeeDetails());
