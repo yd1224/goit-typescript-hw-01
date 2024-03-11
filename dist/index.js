e7d6aac7551a82368bcb2498b28f566b81bb27a5
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-03-11 16:57:47 +0100

    finish two tasks

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,28 @@
-import { concatenation } from './concatenation';
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-if (button && input) {
-    button.addEventListener('click', () => {
-        concatenation(input.value, 'hello!');
-    });
+// import { concatenation } from './concatenation';
+// const button = document.querySelector('button')!;
+// const input = document.querySelector('input')!;
+// if (button && input) {
+//   button.addEventListener('click', () => {
+//     concatenation(input.value, 'hello!');
+//   });
+// }
+class Employee {
+    constructor(name, department, salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+    getEmployeeDetails() {
+        return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    }
 }
+class Manager extends Employee {
+    // Реалізуйте конструктор та збільшіть salary на 10000
+    constructor(name, department, salary) {
+        super(name, department, salary);
+        this.salary += 1000;
+    }
+}
+const manger = new Manager("Ajax", "Business", 0);
+console.log(manger.getEmployeeDetails());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
-import { concatenation } from './concatenation';
+// import { concatenation } from './concatenation';
 
-const button = document.querySelector('button')!;
-const input = document.querySelector('input')!;
+// const button = document.querySelector('button')!;
+// const input = document.querySelector('input')!;
 
-if (button && input) {
-  button.addEventListener('click', () => {
-    concatenation(input.value, 'hello!');
-  });
+// if (button && input) {
+//   button.addEventListener('click', () => {
+//     concatenation(input.value, 'hello!');
+//   });
+// }
+
+class Employee {
+  // Заповніть модифікатори доступу
+ public name: string;
+ private department: string;
+ protected salary: number;
+
+  constructor(name: string, department: string, salary: number) {
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+  }
+
+  getEmployeeDetails() {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
 }
+
+class Manager extends Employee {
+  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor( name: string, department: string, salary: number){
+    super(name, department, salary)
+    this.salary += 1000;
+  }
+}
+const manger=new Manager("Ajax", "Business", 0)
+console.log(manger.getEmployeeDetails());
